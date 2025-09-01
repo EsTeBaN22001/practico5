32b4f86c8db03902449aa1ccea2f686d3b0625fc
Esteban <esteban1.redon2@gmail.com>
2025-09-01 20:10:11 -0300

    Se agregó la estructura de carpetas del proyecto

--- src/Controllers/Contacto.java
+++ src/Controllers/Contacto.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Controllers;
 
 /**
  *
  * @author esteb
  */
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("El pepe");
-    }
+public class Contacto {
 
 }
