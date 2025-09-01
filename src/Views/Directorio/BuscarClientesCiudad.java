c7a8978bd482bdb800fe8694205d524eabaa6e69
Gunxo116 <yamilbarbeito10@gmail.com>
2025-09-01 20:33:45 -0300

    Todas las views

---
+++ src/Views/Directorio/BuscarClientesCiudad.java
@@ -0,0 +1,42 @@
+
+package Views.Directorio;
+
+
+
+
+public class BuscarClientesCiudad extends javax.swing.JInternalFrame {
+    
+    
+    public BuscarClientesCiudad() {
+        initComponents();
+        this.setSize(500, 500);
+        
+        
+        
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 488, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 464, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+
+
+
+}
